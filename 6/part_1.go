1b52029ba7e14f929b9c0b1ece37ba80c8a7b4f8
jack-barr3tt <jjbdev21@gmail.com>
2022-12-07 15:14:28 +0000

    Day 6

---
+++ 6/part_1.go
@@ -0,0 +1,43 @@
+package main
+
+import "os"
+
+func main() {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	q := make([]byte, 0)
+
+	for i := range data {
+		q = append(q, data[i])
+		if len(q) <= 4 {
+			continue
+		}else{
+			q = q[1:]
+		}
+
+		same := false
+
+		for j := range q {
+			for k := range q {
+				if j == k {
+					continue
+				}
+				if q[j] == q[k] {
+					same = true
+					break
+				}
+			}
+			if same {
+				break
+			}
+		}
+
+		if !same {
+			println(i + 1)
+			break
+		}
+	}
+}
